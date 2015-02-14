7ed705ec98763606d30e12d427eb110d58c65023
Melissa <melissa.a.voegeli@gmail.com>
2015-02-14 12:25:12 -0600

    soltion

--- atoms-solved.clj
+++ atoms-solved.clj
@@ -1,7 +1,7 @@
 (def my-account (atom {:checking 5}))
 
 (defn balance [account] (str "Balance: $" (:checking @my-account)))
-(defn check-and-withdraw [account val]
+(defn withdraw [account val]
     (let [known-balance (balance account)]
       (Thread/sleep 1000)
       (let [new-account (if (<= val (:checking account) )
@@ -11,10 +11,9 @@
                               account))]
            (println (str "Known " known-balance ", Current Balance: $" (:checking new-account)))
            new-account)))
-(defn withdraw [account val] (swap! my-account check-and-withdraw val))
 (doseq [i (range 10)]
  (do
-    (future (withdraw my-account 1)) ; this shows up the synchronousness of compare-and-set
+    (future (swap! my-account withdraw 1)) ; this shows up the synchronousness of compare-and-set
     (println "Tried to withdraw $1")))
 
 (balance my-account)
